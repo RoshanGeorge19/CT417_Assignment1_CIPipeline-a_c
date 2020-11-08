b9c1183cf9d48a998d28c8f921c64acbe07eb241
Roshan George <r.george5@nuigalway.ie>
2020-11-08 18:22:35 +0000

    StudentClass init

--- src/main/java/com/cipipeline/Student.java
+++ src/main/java/com/cipipeline/Student.java
@@ -1,5 +1,54 @@
 package com.cipipeline;
 
+
 public class Student {
+	private String name, dob;
+	private int age, id;
+	// modules enrolled
+	// Programme course
+	
+	public Student(String name, String dob, int age, int id) {
+		this.setName(name);
+		this.setDob(dob);
+		this.setAge(age);
+		this.setId(id);
+		// modules enrolled
+	}
+	
+	// Accessor & Mutator Methods
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
 
 }
