9504a1d54548dd8b3815da6de87e451379498cc2
Roshan George <r.george5@nuigalway.ie>
2020-11-10 17:22:56 +0000

    CourseProgJunit

--- src/test/java/com/cipipeline/CourseProgrammeJUnitTest.java
+++ src/test/java/com/cipipeline/CourseProgrammeJUnitTest.java
@@ -1,53 +1,44 @@
-/**
- * 
- */
 package com.cipipeline;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
+import java.util.List;
+
+import org.joda.time.DateTime;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-/**
- * @author Roshan George
- *
- */
+
+
 public class CourseProgrammeJUnitTest {
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
+	private Student student_1;
+	private Student student_2;
+	private Module module_1;
+	private CourseProgramme course_1;
+	private CourseProgramme course_2;
+	
 	@Before
 	public void setUp() throws Exception {
+		student_1 = new Student("MadaYehaf", "01-02-00", 21, 17672317);
+		student_2 = new Student("MarkRober", "09-09-00", 21, 13245679);
+	
+		module_1 = new Module("EE451");		
+		course_1 = new CourseProgramme("BP", new DateTime("2019-07-01T00:00:00Z"), new DateTime("2029-07-01T00:00:00Z"));
+		course_2 = new CourseProgramme("BL", new DateTime("2009-07-01T00:00:00Z"), new DateTime("2029-07-01T00:00:00Z"));
 	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
+	@Test
+	public void remove_student() {
+		course_1.addStudentCourse(student_1);
+		List<Student> currentResult = course_1.getStudentList();
+		assert(currentResult.contains(student_1));
+		assertFalse(currentResult.contains(student_2));
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void remove_module() {
+		course_1.addModuleCourse(module_1);
+		course_1.removeModuleCourse(module_1);
+		List<Module> currentResults = course_1.getModulesList();
+		assert(currentResults.contains(module_1));
 	}
-
 }
--- src/test/java/com/cipipeline/ModuleJUnitTest.java
+++ src/test/java/com/cipipeline/ModuleJUnitTest.java
@@ -11,7 +11,6 @@ public class ModuleJUnitTest {
 	private Student student_1;
 	private Student student_2;
 	private Module module_1;
-	private Module module_2;
 	private CourseProgramme course_1;
 	private CourseProgramme course_2;
 
@@ -21,10 +20,8 @@ public class ModuleJUnitTest {
 	public void setUp() throws Exception {
 		student_1 = new Student("MadaYehaf", "01-02-00", 21, 17672317);
 		student_2 = new Student("MarkRober", "09-09-00", 21, 13245679);
-		
-		module_1 = new Module("EE451");
-		module_2 = new Module("CT417");
-		
+	
+		module_1 = new Module("EE451");		
 		course_1 = new CourseProgramme("BP", new DateTime("2019-07-01T00:00:00Z"), new DateTime("2029-07-01T00:00:00Z"));
 		course_2 = new CourseProgramme("BL", new DateTime("2009-07-01T00:00:00Z"), new DateTime("2029-07-01T00:00:00Z"));
 	}
