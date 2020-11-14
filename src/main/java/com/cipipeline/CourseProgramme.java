2929e06711bb299c7748d8645c56d1a646df0734
Roshan George <r.george5@nuigalway.ie>
2020-11-14 01:47:11 +0000

    update toString CoursePrg

--- src/main/java/com/cipipeline/CourseProgramme.java
+++ src/main/java/com/cipipeline/CourseProgramme.java
@@ -6,45 +6,45 @@ import java.util.List;
 import org.joda.time.DateTime;
 
 public class CourseProgramme {
-	
+
 	private String courseName;
 	private DateTime startDate, endDate;
 	private List<Module> modulesList;
 	private List<Student> studentList;
-	
+
 	public CourseProgramme(String courseName, DateTime startDate, DateTime endDate) {
 		this.setCourseName(courseName);
 		this.setStartDate(startDate);
 		this.setEndDate(endDate);
 		this.modulesList = new ArrayList<Module>();
 		this.studentList = new ArrayList<Student>();
-				
+
 	}
-	
+
 	public void addStudentCourse(Student student) {
-		if(!studentList.contains(student)) {
+		if (!studentList.contains(student)) {
 			studentList.add(student);
 			student.setCoursePrg(this);
 		}
-		
+
 	}
-	
+
 	public void removeStudentCourse(Student student) {
-		if(studentList.contains(student)) {
+		if (studentList.contains(student)) {
 			studentList.remove(student);
 			student.removeStudentCourse();
 		}
 	}
-	
+
 	public void addModuleCourse(Module module) {
-		if(!modulesList.contains(module)) {
+		if (!modulesList.contains(module)) {
 			modulesList.add(module);
 			module.addCourse(this);
 		}
 	}
-	
+
 	public void removeModuleCourse(Module module) {
-		if(modulesList.contains(module)) {
+		if (modulesList.contains(module)) {
 			modulesList.remove(module);
 			module.removeCourse(this);
 		}
@@ -90,30 +90,33 @@ public class CourseProgramme {
 	public void setStudentList(List<Student> studentList) {
 		this.studentList = studentList;
 	}
-	
+
 	public String getElementModulesList() {
 		StringBuilder sbML = new StringBuilder();
-		for(Module module : modulesList) {
-			sbML.append(module.getId());
+		for (Module module : modulesList) {
+			sbML.append(module.getId() + "; ");
 		}
 		String str = sbML.toString();
 		return str;
 	}
-	
+
 	public String getElementStudentList() {
 		StringBuilder sbSL = new StringBuilder();
-		for(Student student : studentList) {
+		for (Student student : studentList) {
 			sbSL.append(student.getUsername());
 		}
 		String str = sbSL.toString();
 		return str;
 	}
-	
+
 	@Override
 	public String toString() {
-		String str = this.courseName +"\n"+ this.getStartDate() +"\n"+ this.getEndDate()+"\n"+getElementModulesList()+"\n"+getElementStudentList();
+		String str = "Course Name: " + "\n" + this.courseName + 
+					 "Start Date: " + "\n" + this.getStartDate() + 
+					 "End Date: " + "\n" + this.getEndDate() +
+					 "Modules in Course" + "\n" + getElementModulesList() + 
+					 "Students Enrolled: " + "\n" + getElementStudentList();
 		return str;
 	}
-	
 
 }
